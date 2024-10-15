5c761e8a300bb754dccc6cfb3b94c54b50841049
Kurmash Nurzhan <65040436+enzhas@users.noreply.github.com>
2024-10-15 14:13:31 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,30 +2,24 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.wsgi import get_wsgi_application  # Import here
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qflexProject.settings')
     try:
-        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
+    application = get_wsgi_application()  # Call the WSGI application
     main()
-    
-application = get_wsgi_application()
 
+# This function creates a WSGI app for Vercel
 def vercel_app(environ, start_response):
-    from wsgi import application
     return application(environ, start_response)
-
-# This line is crucial
-# handler.app = vercel_app
